8150c1df3d604b67fb4f72c8fc96221177655fcc
SVKP <SVKP@DESKTOP-EM2K1LL.bbrouter>
2023-09-27 15:48:43 +0530

    first commit

---
+++ project11/src/com/tns/program/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.program;
+//program to demonstrate encapsulation in java
+public class Encapsulation {
+//data members
+	public int serialno;
+	public String name;
+	public int age;
+	
+	//memberfunction
+	public void show() {
+		System.out.println("serialno"+ serialno+ "\t"+" name"+name+ "\t"+ "age"+age);
+	}
+
+}
