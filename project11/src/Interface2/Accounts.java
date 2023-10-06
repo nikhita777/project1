1f00b778ccb5d99a311e489a9f1bf3ddfdfe6052
SVKP <SVKP@DESKTOP-EM2K1LL.bbrouter>
2023-10-06 16:44:25 +0530

    first commit

---
+++ project11/src/Interface2/Accounts.java
@@ -0,0 +1,8 @@
+package Interface2;
+
+
+
+public interface Accounts {
+	void deliveryCharges();// abstract method
+	//int a;//can't declare instance varible
+}
---
+++ project11/src/Interface2/Prime.java
@@ -0,0 +1,18 @@
+package Interface2;
+
+	public class Prime  implements Accounts{
+		
+		
+		
+
+		@Override
+		public void deliveryCharges() {
+			int amt=0;
+			System.out.println("the delivery charge for prime account is"+ amt);
+			
+
+		}
+
+	}
+
+
